8ce5017cbee6b326e52d0ac662af75f9f3de37bd
takeo <takeofujita@gmail.com>
2013-01-14 05:02:11 +0900

    script for setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git ls-files | while read file; do
+  ln -s $PWD/$file ~
+done
+
+git config --global alias.st status
+git config --global alias.ci 'commit -v'
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global color.ui auto
+git config --global core.excludesfile ~/.gitignore_global
