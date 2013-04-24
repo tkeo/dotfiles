1d3d98a0d42788aaa6c48b509ea9e28f4f8c9f98
Takeo Fujita <takeofujita@gmail.com>
2013-04-24 22:51:18 +0900

    add gitconfig

--- setup.sh
+++ setup.sh
@@ -4,13 +4,6 @@ git ls-files | egrep '^\.' | while read file; do
   ln -s $PWD/$file ~
 done
 
-git config --global alias.st status
-git config --global alias.ci 'commit -v'
-git config --global alias.co checkout
-git config --global alias.br branch
-git config --global color.ui auto
-git config --global core.excludesfile ~/.gitignore_global
-
 if [ ! -f $HOME/.git-prompt.sh ]; then
   wget 'https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh' -O $HOME/.git-prompt.sh
 fi
