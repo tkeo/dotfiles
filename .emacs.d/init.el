23299c668155f1d74ec3a74b8dcbbb211acb9043
tkeo <takeofujita@gmail.com>
2018-09-02 17:34:59 +0900

    add ensure

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -38,31 +38,36 @@
   (package-install 'use-package))
 (require 'use-package)
 
-(use-package el-get)
+(use-package el-get
+  :ensure t)
 
 (use-package ag
+  :ensure t
   :defer t
   :init
   (setq ag-highlight-search t)
   (setq ag-reuse-window 't))
 
 (use-package anzu
+  :ensure t
   :defer t
   :init
   (custom-set-variables
    '(anzu-deactivate-region t)
    '(anzu-mode-lighter "")
    '(anzu-search-threshold 1000))
-  (global-anzu-mode +1)
   :bind (("M-%" . anzu-query-replace)
 	 ("C-M-%" . 'anzu-query-replace-regexp))
   :config
+  (global-anzu-mode +1)
   (set-face-attribute 'anzu-mode-line nil
 		      :foreground "black" :weight 'bold))
 
-(use-package bundler)
+(use-package bundler
+  :ensure t)
 
 (use-package company
+  :ensure t
   :diminish
   :init
   (add-hook 'after-init-hook 'global-company-mode)
@@ -72,6 +77,7 @@
 	      ("C-h" . nil)))
 
 (use-package csharp-mode
+  :ensure t
   :no-require t
   :config
   (add-hook 'csharp-mode-hook
@@ -80,14 +86,15 @@
 	       (setq c-basic-offset 4)))
   (modify-coding-system-alist 'file "\\.cs\\'" 'utf-8-with-signature-unix))
 
-(use-package diminish)
+(use-package diminish
+  :ensure t)
 
 (use-package elscreen
+  :ensure t
   :init
   (setq elscreen-prefix-key (kbd "C-c e"))
   (setq elscreen-tab-display-kill-screen nil)
   (setq elscreen-tab-display-control nil)
-  (elscreen-start)
   :bind (("C-c 0" . elscreen-jump-0)
 	 ("C-c 1" . elscreen-jump)
 	 ("C-c 2" . elscreen-jump)
@@ -97,9 +104,12 @@
 	 ("C-c 6" . elscreen-jump)
 	 ("C-c 7" . elscreen-jump)
 	 ("C-c 8" . elscreen-jump)
-	 ("C-c 9" . elscreen-jump-9)))
+	 ("C-c 9" . elscreen-jump-9))
+  :config
+  (elscreen-start))
 
 (use-package enh-ruby-mode
+  :ensure t
   :custom
   (enh-ruby-add-encoding-comment-on-save nil "保存時にマジックコメントを追加しない")
   (enh-ruby-deep-indent-paren nil "かっこを複数行に書くときのインデントを深くしない")
@@ -110,19 +120,23 @@
 	 ("Rakefile\\'" . enh-ruby-mode)))
 
 (use-package expand-region
+  :ensure t
   :bind ("C-," . er/expand-region))
 
 (use-package flycheck
+  :ensure t
   :init
   (setq flycheck-check-syntax-automatically '(mode-enabled save idle-change))
   (setq flycheck-idle-change-delay 2))
 
 (use-package git-gutter
+  :ensure t
   :diminish
   :config
   (global-git-gutter-mode))
 
 (use-package helm
+  :ensure t
   :defer t
   :diminish
   :bind (("M-x" . helm-M-x)
@@ -141,6 +155,7 @@
   (helm-mode 1))
 
 (use-package helm-ag
+  :ensure t
   :defer t
   :init
   (custom-set-variables
@@ -149,9 +164,11 @@
   :bind ("C-c s" . helm-ag-project-root))
 
 (use-package helm-ghq
+  :ensure t
   :defer t)
 
 (use-package helm-gtags
+  :ensure t
   :defer t
   :init
   (setq helm-gtags-auto-update t)
@@ -167,13 +184,16 @@
 	      ("M-," . helm-gtags-pop-stack)))
 
 (use-package helm-migemo
+  :ensure t
   :disabled t)
 
 (use-package helm-projectile
+  :ensure t
   :config
   (helm-projectile-on))
 
 (use-package helm-swoop
+  :ensure t
   :defer t
   :bind (("M-s" . helm-swoop)
 	 ("C-c M-s" . helm-multi-swoop)
@@ -183,6 +203,7 @@
 	 ("C-s" . helm-next-line)))
 
 (use-package highlight-symbol
+  :ensure t
   :defer t
   :diminish
   :hook (prog-mode . highlight-symbol-mode)
@@ -190,19 +211,23 @@
   (setq highlight-symbol-idle-delay 0.5))
 
 (use-package loccur
+  :ensure t
   :bind ("C-o" . loccur-current))
 
 (use-package lua-mode
+  :ensure t
   :no-require t
   :init
   (setq lua-indent-level 2))
 
 (use-package lusty-explorer
+  :ensure t
   :disabled t
   :config
   (lusty-explorer-mode 1))
 
 (use-package magit
+  :ensure t
   :defer t
   :bind ("C-c g" . magit-status)
   :config
@@ -214,9 +239,11 @@
 		      :inherit 'default))
 
 (use-package markdown-mode
+  :ensure t
   :no-require t)
 
 (use-package migemo
+  :ensure t
   :defer t
   :init
   (setq migemo-command "cmigemo")
@@ -229,20 +256,24 @@
   (migemo-init))
 
 (use-package mwim
+  :ensure t
   :bind (("C-a" . mwim-beginning-of-code-or-line)
 	 ("C-e" . mwim-end-of-code-or-line)))
 
 (use-package omnisharp
+  :ensure t
   :bind (("M-." . omnisharp-auto-complete)
 	 ("C-c d" . omnisharp-go-to-definition))
   :init
   (setq omnisharp-server-executable-path (expand-file-name "~/.ghq/github.com/OmniSharp/omnisharp-server/OmniSharp/bin/Debug/OmniSharp.exe")))
 
 (use-package popwin
-  :init
+  :ensure t
+  :config
   (setq display-buffer-function 'popwin:display-buffer))
 
 (use-package powerline
+  :ensure t
   :config
   (set-face-attribute 'mode-line nil
 		      :foreground "#fff"
@@ -259,19 +290,24 @@
   (powerline-default-theme))
 
 (use-package projectile
+  :ensure t
   :diminish
-  :init
+  :config
   (projectile-global-mode))
 
 (use-package projectile-rails
+  :ensure t
   :config
   (projectile-rails-global-mode))
 
-(use-package rails-log-mode)
+(use-package rails-log-mode
+  :ensure t)
 
-(use-package recentf-ext)
+(use-package recentf-ext
+  :ensure t)
 
 (use-package robe
+  :ensure t
   :diminish
   :init
   ;(add-hook 'enh-ruby-mode-hook 'robe-mode)
@@ -279,6 +315,7 @@
     '(push 'company-robe company-backends)))
 
 (use-package rspec-mode
+  :ensure t
   :no-require t)
 
 (el-get-bundle juszczakn/ruby-block)
@@ -290,6 +327,7 @@
   (ruby-block-mode t))
 
 (use-package ruby-electric
+  :ensure t
   :no-require t
   :hook (enh-ruby-mode . ruby-electric-mode))
 
@@ -320,43 +358,55 @@
 	(when (> offset 0) (forward-char offset))))))
 
 (use-package sass-mode
+  :ensure t
   :no-require t)
 
 (use-package scss-mode
+  :ensure t
   :no-require t)
 
 (use-package slim-mode
+  :ensure t
   :no-require t)
 
 (use-package smart-newline
+  :ensure t
   :bind ("C-m" . smart-newline))
 
 (use-package ssh-config-mode
+  :ensure t
   :no-require t
   :mode (".ssh/config\\'" . ssh-config-mode))
 
-(use-package swap-buffers)
+(use-package swap-buffers
+  :ensure t)
 
-(use-package terraform-mode)
+(use-package terraform-mode
+  :ensure t)
 
 (use-package volatile-highlights
+  :ensure t
   :diminish
   :config
   (volatile-highlights-mode t))
 
 (use-package web-mode
+  :ensure t
   :no-require t
   :mode (("\\.html?\\'" . web-mode)))
 
 (use-package which-key
+  :ensure t
   :diminish
   :config
   (which-key-mode 1))
 
 (use-package yaml-mode
+  :ensure t
   :no-require t)
 
 (use-package yasnippet
+  :ensure t
   :diminish yas-minor-mode
   :init
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
