2bbd50e9d5a0fdcc841a37871d2e698f8721202c
Takeo Fujita <takeofujita@gmail.com>
2018-10-18 16:31:46 +0900

    rbenv default gems

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ mkdir -pv ~/.config/fish/functions
 mkdir -pv ~/.config/git
 mkdir -pv ~/.config/peco
 mkdir -pv ~/.emacs.d
+mkdir -pv ~/.rbenv
 
 git ls-files | egrep '^\.' | while read file; do
   ln -sv "$PWD/$file" "$HOME/$file"
